c437b0fb8591dff73693c06c235d4d41e6a874ab
mario cervantes <mariocg106@gmail.com>
2021-06-02 10:58:21 +0200

    puesta a punto

--- apiLibreria/src/App/app.php
+++ apiLibreria/src/App/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use Slim\Factory\AppFactory;
-use Slim\Exception\NotFoundException;
+//use Slim\Exception\NotFoundException;
 use Slim\Exception\HttpNotFoundException;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
