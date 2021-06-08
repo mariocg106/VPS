120f8d022c5fd71fd558ef59302be327ce280243
mario cervantes <mariocg106@gmail.com>
2021-06-08 12:36:29 +0200

    sdkjsñd

--- apiLibreria/src/Routes/libros.php
+++ apiLibreria/src/Routes/libros.php
@@ -8,3 +8,5 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 $app->get('/api/libros','App\Controllers\LibrosController:getAll');
     
 $app->get('/api/libros/new','App\Controllers\LibrosController:new') ;
+
+$app->get('/api/libros/new1','App\Controllers\LibrosController:new') ;
