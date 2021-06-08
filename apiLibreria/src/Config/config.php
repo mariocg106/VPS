319bc1977a306dd8bf4d5aad8812b6f4708c9d01
mario cervantes <mariocg106@gmail.com>
2021-06-08 10:14:16 +0200

    e

--- apiLibreria/src/Config/config.php
+++ apiLibreria/src/Config/config.php
@@ -6,7 +6,7 @@ interface config {
     CONST DB_HOST = "localhost";
     CONST DB_PORT = "33060";
     
-    CONST DB_USER = "root";
-    CONST DB_PASSWD = "";
+    CONST DB_USER = "mario";
+    CONST DB_PASSWD = "Vivaloprivado1@";
     CONST DB_CHARSET = "utf8";
 }
