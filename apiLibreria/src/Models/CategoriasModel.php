bb4e3f31563ab688cb1f7a0cdf63a2c05b0e42c4
mario cervantes <mariocg106@gmail.com>
2021-06-09 09:04:34 +0200

    newCat

--- apiLibreria/src/Models/CategoriasModel.php
+++ apiLibreria/src/Models/CategoriasModel.php
@@ -1,41 +1,20 @@
 <?php
-echo "model"
-namespace App\Model;
+namespace App\Models;
 use App\Config\DB;
-use Exception;
 
-//definimos LibrosModel como una clase estática:
-//no se puede hacer un new, no hay $this, no hay método __contruct()
 class CategoriasModel {
-    private static $table = 'categorias';
     private static $DB;
 
     public static function conexionDB(){
         CategoriasModel::$DB = new DB();
     }
-    
-    public static function new($parametros){
-        try{
-            $valores = array_values($parametros);
-            CategoriasModel::conexionDB();
-            $sql = "insert into categorias (categoriaid, nombre_categoria) 
-                    values (?, ?)";
-            $data = CategoriasModel::$DB->run($sql, $valores);
-            return "Categoria ". $parametros['nombre_categoria'] . " insertado correctamente ";
-        }catch(Exception $e){
-            return $e->getMessage();
-         }
-    }
+
     public static function getAll(){
         CategoriasModel::conexionDB();
-        $sql = "Select * from categorias";
+        $sql = "SELECT * from categorias";
         $data = CategoriasModel::$DB->run($sql, []);
         return $data->fetchAll();
+        //var_dump ($data->fetchAll());
     }
-    public static function show($param){
-        LibrosModel::conexionDB();
-        $sql = 'SELECT * from libros where libro_id = ?';
-        $data = CategoriasModel::$DB->run($sql, $param);
-        return $data->fetch();
-    }
+
 }
