f3f65e02adb9eb28547c6f46e261e934c84fe375
mario cervantes <mariocg106@gmail.com>
2021-06-09 08:54:44 +0200

    newCat

--- apiLibreria/src/Controllers/CategoriasController.php
+++ apiLibreria/src/Controllers/CategoriasController.php
@@ -1,40 +1,35 @@
 <?php
-namespace App\Model;
-use App\Config\DB;
-use Exception;
+    namespace App\Controllers;
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use App\Model\CategoriasModel;    
 
-//definimos LibrosModel como una clase estática:
-//no se puede hacer un new, no hay $this, no hay método __contruct()
-class CategoriasModel {
-    private static $table = 'categorias';
-    private static $DB;
+    class CategoriasController {
+        public function new(Request $request, response $response, $args){
+            $parametros = $request->getParsedBody();
+        //    $parametros = $request->getBody()->getContents();
+            $parametros = (array)json_decode($request->getBody()->getContents());
+           // $result = UsuariosModel::new($parametros); 
+            $categoriaid = $parametros['categoriaid'];
+            $nombre = $parametros['nombre_categoria'];
+            $valores = array($categoriaid, $nombre);
+            $resultado = CategoriasModel::new($parametros);
+            $dataJson = json_encode(array(
+                'status'=> 'exit', 
+                '' => 'Registro insertado con éxito',
+                'data' => $parametros));
+            $response->getBody()->write($dataJson);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
 
-    public static function conexionDB(){
-        CategoriasModel::$DB = new DB();
+        }
+        public function getAll($request, $response, $args){
+            $categorias = CategoriasModel::getAll();
+            $categoriasjson = json_encode($categorias);
+            $response->getBody()->write($categoriasjson);
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+        }
     }
-    
-    public static function new($parametros){
-        try{
-            $valores = array_values($parametros);
-            CategoriasModel::conexionDB();
-            $sql = "insert into categorias (categoriaid, nombre_categoria) 
-                    values (?, ?)";
-            $data = CategoriasModel::$DB->run($sql, $valores);
-            return "Categoria ". $parametros['nombre_categoria'] . " insertado correctamente ";
-        }catch(Exception $e){
-            return $e->getMessage();
-         }
-    }
-    public static function getAll(){
-        CategoriasModel::conexionDB();
-        $sql = "Select * from categorias";
-        $data = CategoriasModel::$DB->run($sql, []);
-        return $data->fetchAll();
-    }
-    public static function show($param){
-        LibrosModel::conexionDB();
-        $sql = 'SELECT * from libros where libro_id = ?';
-        $data = CategoriasModel::$DB->run($sql, $param);
-        return $data->fetch();
-    }
-}
