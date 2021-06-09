a45eb5faa3325fcd3e25585e7fc51b6fd2861133
mario cervantes <mariocg106@gmail.com>
2021-06-09 09:25:18 +0200

    newCat

--- apiLibreria/src/Controllers/CategoriasController.php
+++ apiLibreria/src/Controllers/CategoriasController.php
@@ -1,35 +1,26 @@
 <?php
-    namespace App\Controllers;
-    use Psr\Http\Message\ResponseInterface as Response;
-    use Psr\Http\Message\ServerRequestInterface as Request;
-    use App\Model\CategoriasModel;    
+namespace App\Controllers;
 
-    class CategoriasController {
-        public function new(Request $request, response $response, $args){
-            $parametros = $request->getParsedBody();
-        //    $parametros = $request->getBody()->getContents();
-            $parametros = (array)json_decode($request->getBody()->getContents());
-           // $result = UsuariosModel::new($parametros); 
-            $categoriaid = $parametros['categoriaid'];
-            $nombre = $parametros['nombre_categoria'];
-            $valores = array($categoriaid, $nombre);
-            $resultado = CategoriasModel::new($parametros);
-            $dataJson = json_encode(array(
-                'status'=> 'exit', 
-                '' => 'Registro insertado con éxito',
-                'data' => $parametros));
-            $response->getBody()->write($dataJson);
-            return $response
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\CategoriasModel;
+
+class CategoriasController {
+
+    public function getAll(Request  $request, Response $response, $args){
+        $categorias = CategoriasModel::getAll();
+        $categorias_JSON = json_encode($ategorias);
+        $response->getBody()->write($ategorias_JSON);
+        return $response
                 ->withHeader('Content-Type', 'application/json')
                 ->withStatus(200);
-
-        }
-        public function getAll($request, $response, $args){
-            $categorias = CategoriasModel::getAll();
-            $categoriasjson = json_encode($categorias);
-            $response->getBody()->write($categoriasjson);
-            return $response
-                ->withHeader('Content-Type', 'application/json')
-                ->withStatus(200);
-        }
+       
     }
+
+    public function new(Request  $request, Response $response, $args){
+        $response->getBody()->write("Categoria insertada correctamente");
+        return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+    }
+}
